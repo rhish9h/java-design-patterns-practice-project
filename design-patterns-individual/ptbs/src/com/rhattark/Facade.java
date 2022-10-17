b81244634bca1022d4500501f63ef0e76fe8c7a4
Rhishabh Hattarki <rhattark@asu.edu>
2022-10-16 20:46:07 -0700

    basic shell for facade

---
+++ design-patterns-individual/ptbs/src/com/rhattark/Facade.java
@@ -0,0 +1,57 @@
+package com.rhattark;
+
+public class Facade {
+    private int userType;
+    private Product theSelectedProduct;
+    private int nProductCategory;
+    private ClassProductList theProductList;
+    private Person thePerson;
+
+    public boolean login() {
+        return false;
+    }
+
+    public void addTrading() {
+
+    }
+
+    public void viewTrading() {
+
+    }
+
+    public void decideBidding() {
+
+    }
+
+    public void discussBidding() {
+
+    }
+
+    public void submitBidding() {
+
+    }
+
+    public void remind() {
+
+    }
+
+    public void createUser(UserInfoItem userInfoItem) {
+
+    }
+
+    public void createProductList() {
+
+    }
+
+    public void attachProductToUser() {
+
+    }
+
+    public Product selectProduct() {
+        return null;
+    }
+
+    public void productOperation() {
+
+    }
+}
---
+++ design-patterns-individual/ptbs/src/com/rhattark/Person.java
@@ -0,0 +1,4 @@
+package com.rhattark;
+
+public interface Person {
+}
